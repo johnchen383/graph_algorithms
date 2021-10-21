5c9095e734451f719668302b281a0ceb7c3966b9
John Chen <john.bm.chen@gmail.com>
2021-10-21 19:35:27 +1300

    feat: nodeDistComparator implemented for pq. Dikstra unfinished

--- SSSP/Dijkstra.java
+++ SSSP/Dijkstra.java
@@ -4,22 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 
+import comparators.NodeDistComparator;
 import enums.Colour;
 import models.graphs.Graph;
 import models.nodes.Node;
 
 public class Dijkstra {
     public static void run(Graph graph, Node s){
-        PriorityQueue<Node> pq = new PriorityQueue<Node>();
         Map<Node, Colour> colour = new HashMap<Node, Colour>(graph.getOrder());
         Map<Node, Integer> dist = new HashMap<Node, Integer>(graph.getOrder());
+        PriorityQueue<Node> pq = new PriorityQueue<Node>(graph.getOrder(), new NodeDistComparator(dist));
 
         //initialise
         for (Node u: graph.getNodeSet()){
             colour.put(u, Colour.WHITE);
         }
         colour.replace(s, Colour.GREY);
-        // pq.add(s, 0);
+        pq.add(s);
 
         while (!pq.isEmpty()){
             Node u = pq.peek();
---
+++ comparators/NodeDistComparator.java
@@ -0,0 +1,20 @@
+package comparators;
+
+import java.util.Comparator;
+import java.util.Map;
+
+import models.nodes.Node;
+
+public class NodeDistComparator implements Comparator<Node>{
+    Map<Node, Integer> dist;
+
+    public NodeDistComparator(Map<Node, Integer> dist){
+        this.dist = dist;
+    }
+
+    @Override
+    public int compare(Node n1, Node n2) {
+        return dist.get(n1) - dist.get(n2);
+    }
+    
+}
